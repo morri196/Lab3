918df1cbde0cd8edc9af122492fb8e59b45ba324
Kari Mcfarlin <karimcfarlin@Karis-MacBook-Air.local>
2018-07-18 09:07:21 -0400

    initialize lab3

---
+++ src/Lab3/EvenOrOdd.java
@@ -0,0 +1,24 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+	public static void main(String [] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		System.out.println("Enter a number 1 through 100: ");
+		int num = scnr.nextInt();
+		
+		if ( num % 2 == 0 && num > 2 && num <= 25)
+	         System.out.println(num + " is even and less than 25");
+		else if (num % 2 == 0 && num >= 26 && num <= 60)
+			System.out.println(num + " is Even");
+		else if (num % 2 == 0 && num > 60)
+			System.out.println(num + " is Even");
+		else if (num % 2 != 0 && num > 60)
+			System.out.println(num + " is odd and over 60.");
+	      else
+	         System.out.println(num + " is odd.");
+		
+	}
+}
